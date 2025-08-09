0ab8343d00547f166ea12d936ef696cfa35ea748
nihad217 <nihadoo8888@gmail.com>
2025-08-09 15:40:45 +0200

    added the methode for calibration and intergrated it into code

--- implementation code/experiment.py
+++ implementation code/experiment.py
@@ -1,69 +1,67 @@
-import time
-import tkinter as tk
-from tkinter import *
-from PIL import Image, ImageTk
-import random
 import sys
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
+import random
+from PIL import Image, ImageTk
+from tkinter import Tk, Frame, Label, Button
+from PySide6.QtCore import Qt, QRect, QMargins
+from PySide6.QtGui import QPainter, QColor, QPixmap, QImage
+from PySide6.QtWidgets import QApplication, QWidget
 from pupil_labs.real_time_screen_gaze import marker_generator
 
 
-
 class MarkerWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlag(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.X11BypassWindowManagerHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.marker_size = 200
+        self.pixmaps = [self.createMarker(i) for i in range(4)]
+        self.visibleMarkerIds = []
 
- def __init__(self):
-     super().__init__()
-     self.setWindowFlag(Qt.FramelessWindowHint| Qt.WindowStaysOnTopHint|Qt.X11BypassWindowManagerHint)
-     self.setAttribute(Qt.WA_TranslucentBackground)
-     self.marker_size=200
-     self.pixmaps=[self.createMarker(i) for i in range(4)]
-     self.visibleMarkerIds=[]
+        screen = QApplication.primaryScreen().geometry()
+        self.setGeometry(0, 0, screen.width(), screen.height())
+        self.hide()
 
-     screen=QApplication.primaryScreen().geometry()
-     self.setGeometry(0,0,screen.width(),screen.height())
-     self.show()
- def createMarker(self, marker_id):
-    marker=marker_generator.generate_marker(marker_id, flip_x=True, flip_y=True)
-    image=QImage(10,10,QImage.Format_Mono)
-    image.fill(1)
-    for y in range(marker.shape[0]):
-        for x in range(marker.shape[1]):
-            color= marker[y][x]//255
-            image.setPixel(x+1,y+1,color)
-    return QPixmap.fromImage(image)
- def getCornerRect(self,cornerIdx):
-     padding = 5  # Abstand vom Rand
-     if cornerIdx == 0:  # Top-left
-        return QRect(padding, padding, self.marker_size, self.marker_size)
-     elif cornerIdx == 1:  # Top-right
-        return QRect(self.width()-self.marker_size-padding, padding,
-                  self.marker_size, self.marker_size)
-     elif cornerIdx == 2:  # Bottom-right
-        return QRect(self.width()-self.marker_size-padding,
-                  self.height()-self.marker_size-padding,
-                  self.marker_size, self.marker_size)
-     elif cornerIdx == 3:  # Bottom-left
-        return QRect(padding, self.height()-self.marker_size-padding,
-                  self.marker_size, self.marker_size)
- def paintEvent(self, event):
-    painter = QPainter(self)
-    for cornerIdx in range(4):
-        cornerRect = self.getCornerRect(cornerIdx)
-        if cornerIdx not in self.visibleMarkerIds:
-            painter.fillRect(cornerRect.marginsAdded(QMargins(5, 5, 5, 5)), QColor(255, 0, 0))
-        painter.drawPixmap(cornerRect, self.pixmaps[cornerIdx])
-        painter.fillRect(cornerRect, QColor(0, 0, 0, 128))
+    def createMarker(self, marker_id):
+        marker = marker_generator.generate_marker(marker_id, flip_x=True, flip_y=True)
+        image = QImage(marker.shape[1], marker.shape[0], QImage.Format_Mono)
+        for y in range(marker.shape[0]):
+            for x in range(marker.shape[1]):
+                color = marker[y][x] // 255
+                image.setPixel(x, y, color)
+        return QPixmap.fromImage(image)
 
- def showMarkerFeedback(self, markerIds):
-    self.visibleMarkerIds = markerIds
-    self.repaint()
+    def getCornerRect(self, cornerIdx):
+        padding = 5
+        if cornerIdx == 0:
+            return QRect(padding, padding, self.marker_size, self.marker_size)
+        elif cornerIdx == 1:
+            return QRect(self.width() - self.marker_size - padding, padding, self.marker_size, self.marker_size)
+        elif cornerIdx == 2:
+            return QRect(self.width() - self.marker_size - padding, self.height() - self.marker_size - padding,
+                         self.marker_size, self.marker_size)
+        elif cornerIdx == 3:
+            return QRect(padding, self.height() - self.marker_size - padding, self.marker_size, self.marker_size)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        for cornerIdx in range(4):
+            cornerRect = self.getCornerRect(cornerIdx)
+            if cornerIdx not in self.visibleMarkerIds:
+                painter.fillRect(cornerRect.marginsAdded(QMargins(5, 5, 5, 5)), QColor(255, 0, 0))
+            painter.drawPixmap(cornerRect, self.pixmaps[cornerIdx])
+            painter.fillRect(cornerRect, QColor(0, 0, 0, 128))
+
+    def showMarkers(self):
+        self.show()
+        self.repaint()
+
+    def hideMarkers(self):
+        self.hide()
 
 
 class ExperimentApp:
-
-    def __init__(self):
+    def __init__(self, marker_window):
+        self.marker_window = marker_window
         self.welcome = Tk()
         self.welcome.title("The visual guidance experiment!")
         self.welcome.attributes('-fullscreen', True)
@@ -80,6 +78,39 @@ class ExperimentApp:
         self.welcome.bind("<Escape>", lambda e: self.welcome.attributes("-fullscreen", False))
         self.show_frame(self.slides['slide1'])
         self.welcome.mainloop()
+
+    def calibrate_glasses(self):
+        print("Kalibrierung gestartet")
+        frameAndGaze=self.device.recieve_matched_scene_video_frame_and_gaze(timeout_seconds=1/15)
+
+        if frameAndGaze is None:
+            print("keine gaze-Daten empfangen! ")
+            self.marker_window.hideMarkers()
+        return
+
+        frame,gaze=frameAndGaze
+        result=self.gazeMapper.process_frame(frame,gaze)
+        if self.surface.uid in result.mapped_gaze:
+            for surface_gaze in result.mapped_gaze[self.surface.uid]:
+                self.offset_x = 0.487 - surface_gaze.x
+                self.offset_y=0.370-surface_gaze.y
+        self.marker_window.hideMarkers()
+        print("Kalibrierung fertig!")
+
+    def calibrate_and_continue(self, next_frame):
+        self.marker_window.showMarkers()
+        self.welcome.after(2000, lambda: self.finish_calibration(next_frame))
+
+    def finish_calibration(self, next_frame):
+        print("#")
+        self.calibrate_glasses()
+        print("##")
+        self.marker_window.hideMarkers()
+        print("###")
+        self.show_frame(next_frame)
+        print("####")
+        self.start_counting()
+
     def create_frames(self):
         self.slides = {}
         self.slidePause = Frame(self.welcome)
@@ -93,19 +124,18 @@ class ExperimentApp:
             frame.grid(row=0, column=0, sticky="nsew")
             self.welcome.rowconfigure(0, weight=1)
             self.welcome.columnconfigure(0, weight=1)
+
     def setup_slide1(self):
         label = Label(self.slides['slide1'], text="Willkommen im Experiment!", font=("Segoe UI", 30))
         label.pack(expand=True)
 
         label2 = Label(self.slides['slide1'],
-                       text="Deine Aufgabe ist es auf Bildern Walter zu finden. Vor jedem Bild erscheint ein Countdown, \n"
-                            "der von 3 runterzählt. Direkt nach dem Countdown erscheint das nächste Bild. Viel Glück!!!",
+                       text="Deine Aufgabe ist es auf Bildern Walter zu finden.\nVor jedem Bild erscheint ein Countdown.",
                        font=("Segoe UI", 18))
         label2.pack(expand=True)
 
         try:
-            image = Image.open("pictures/waldo.jpg")
-            image = image.resize((350, 350), Image.LANCZOS)
+            image = Image.open("pictures/waldo.jpg").resize((350, 350))
             photo = ImageTk.PhotoImage(image)
             image_label = Label(self.slides['slide1'], image=photo)
             image_label.image = photo
@@ -114,35 +144,31 @@ class ExperimentApp:
             print("Beispielbild nicht gefunden!")
 
         start_button = Button(self.slides['slide1'], text="Starten", font=("Segoe UI", 20),
-                              command=lambda: [self.show_frame(self.slideCountdown), self.start_counting()])
+                              command=lambda: self.calibrate_and_continue(self.slideCountdown))
         start_button.pack(side="bottom", pady=20)
 
         self.countdown_label = Label(self.slideCountdown, text="", font=("Segoe UI", 40), fg="red")
         self.countdown_label.pack(expand=True)
 
-        pause_label = Label(self.slidePause, text="Zeit ist um. Nun kannst du Pause machen und danach fortfahren",
+        pause_label = Label(self.slidePause, text="Pause. Drücke weiter, um fortzufahren",
                             font=("Segoe UI", 18))
         pause_label.pack(pady=30)
 
         pause_button = Button(self.slidePause, text="Weiter", font=("Segoe UI", 20),
-                              command=lambda: [self.show_frame(self.slideCountdown), self.start_counting()])
+                              command=lambda: self.calibrate_and_continue(self.slideCountdown))
         pause_button.pack(expand=True)
 
-
-
-
     def setup_end_slide(self):
-            end_label = Label(self.slideEnd,
-                              text="Der letzte Timer war ein Witz.\n Du hast es geschafft! Danke für deine Teilnahme!!!",
-                              font=("Segoe UI", 30), fg="green")
-            end_label.pack(expand=True)
-
+        end_label = Label(self.slideEnd,
+                          text="Du hast es geschafft! Danke für deine Teilnahme!",
+                          font=("Segoe UI", 30), fg="green")
+        end_label.pack(expand=True)
 
     def load_images(self):
         for i in range(2, 12):
             try:
                 image_path = f"pictures/whereIsWaldo{i-1}.jpg"
-                img = Image.open(image_path).resize((1000, 1000), Image.LANCZOS)
+                img = Image.open(image_path).resize((1000, 1000))
                 ph = ImageTk.PhotoImage(img)
                 label = Label(self.slides[f'slide{i}'], image=ph)
                 label.image = ph
@@ -156,7 +182,7 @@ class ExperimentApp:
     def start_counting(self, seconds=3):
         if seconds > 0:
             self.countdown_label.config(text=str(seconds))
-            self.slideCountdown.after(1000, lambda: self.start_counting(seconds-1))
+            self.slideCountdown.after(1000, lambda: self.start_counting(seconds - 1))
         else:
             if self.current_index < len(self.image_orders):
                 slide_name = f'slide{self.image_orders[self.current_index]}'
@@ -166,14 +192,9 @@ class ExperimentApp:
             else:
                 self.show_frame(self.slideEnd)
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     marker_window = MarkerWindow()
-    experiment = ExperimentApp()
+    ExperimentApp(marker_window)
     sys.exit(app.exec())
-
-
-
-
-
-
